bca4c969e876b01360650b49e8820e16dc539fda
Elyroma <elynebeaudeau@orange.fr>
2024-03-07 10:42:05 +0100

    refractor: name of calendar

--- src/model/event.ts
+++ src/model/event.ts
@@ -1,8 +1,8 @@
-export interface Calendar {
+export interface Event {
   id: number;
   idUser: number;
   title: string;
   description: string;
   date: Date;
-  repetition: number
+  birthday: boolean;
 }
