6e07ff3449bf4bb588475912449d7134dfbc6aba
Elyroma <elynebeaudeau@orange.fr>
2024-03-10 12:13:30 +0100

    color & title

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "green": "#254d32",
+        "light-green": "#9ebd6e",
+        "dark-green": "#0d1b12",
+        "lavender": "#e3dae7",
+        "light-lavender": "#f6f3f7",
+        "dark-lavender": "#6d507a"
+      },
+    },
   },
   plugins: [],
 }
