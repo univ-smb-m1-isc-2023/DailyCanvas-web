5a975a90ab8822646c639e7c7c4c4bbe761e7b7a
Elyroma <elynebeaudeau@orange.fr>
2024-03-26 22:27:05 +0100

    test and set up

--- src/app/create-challenge/create-challenge.component.ts
+++ src/app/create-challenge/create-challenge.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Renderer2} from '@angular/core';
+import {FormGroup, FormControl, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-create-challenge',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './create-challenge.component.html',
   styleUrl: './create-challenge.component.css'
 })
-export class CreateChallengeComponent {
+export class CreateChallengeComponent implements AfterViewInit{
+  challengeForm = new FormGroup({
+    name: new FormControl(''),
+    description: new FormControl(''),
+    duration: new FormControl(''),
+    interval: new FormControl(''),
+  });
+  constructor(private renderer: Renderer2, private elRef: ElementRef) { }
 
+  ngAfterViewInit(): void {
+    const hostElement = this.elRef.nativeElement;
+    this.renderer.setStyle(hostElement, 'flex-grow', '1');
+  }
+
+  test(){
+    console.log(this.challengeForm)
+  }
 }
