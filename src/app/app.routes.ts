3392daa4160ea4ac712b480391c2b5d0bfff89c9
Elyroma <elynebeaudeau@orange.fr>
2024-04-05 20:10:43 +0200

    cool même le get all marche, va - t - on réussir ce projet ?

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,15 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {AccountComponent} from "./account/account.component";
 import {CreateChallengeComponent} from "./create-challenge/create-challenge.component";
-import {RegisterComponent} from "./connectionPages/register/register.component";
-import {LoginComponent} from "./connectionPages/login/login.component";
 import {ConfidentialityRulesComponent} from "./utils/confidentiality-rules/confidentiality-rules.component";
 import {UtilisationConditionsComponent} from "./utils/utilisation-conditions/utilisation-conditions.component";
+import {FindChallengeComponent} from "./find-challenge/find-challenge.component";
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'account', component: AccountComponent },
   { path: 'challenge/create', component: CreateChallengeComponent},
+  { path: 'challenges', component: FindChallengeComponent},
   { path: 'confidentiality', component: ConfidentialityRulesComponent},
   { path: 'conditions', component: UtilisationConditionsComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
