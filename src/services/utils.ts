d87186ead26a9278325b6f70714dabb3983225d9
Elyroma <elynebeaudeau@orange.fr>
2024-04-16 21:26:50 +0200

    Update utils.ts

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,5 +1,5 @@
-//export const API_URL:string = 'https://api.daily-canvas.oups.net/api';
-export const API_URL:string = 'http://localhost:3000/api';
+export const API_URL:string = 'https://api.daily-canvas.oups.net/api';
+//export const API_URL:string = 'http://localhost:3000/api';
 
 export function isMobileDevice() {
   if( navigator.userAgent.match(/iPhone/i)
